fe3ab5ae4665c81b4cedc0713502772e7c2cb57c
sach10-create <65531346+sach10-create@users.noreply.github.com>
2021-09-25 00:03:38 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -3,18 +3,37 @@ const luckyNumber = document.querySelector("#lucky-number");
 const checkButton = document.querySelector(".check-button");
 const outputBox = document.querySelector(".output-box");
 
-function clickHandler(){
+function clickHandler() {
     var dob = dateOfBirth.value;
-    var sum = prediction(dob)
+    var sum = prediction(dob);
+    var number = luckyNumber.value;
+    if (sum && number) {
+        logic(sum, number);
+    } else {
+        outputBox.innerText = "Please enter both the fields";
+    }
 }
 
-function prediction(dob){
+function logic(sum, number) {
+    var temp = sum % number;
+    if (temp == 0) {
+        displayMsg("WOAHH!! Your Birthday is LUKCY");
+    } else {
+        displayMsg("Unlucky!!");
+    }
+}
+
+function prediction(dob) {
     var sum = 0;
-    dob = dob.replaceAll("-","");
-    for(var i=0;i<dob.length;i++){
+    dob = dob.replaceAll("-", "");
+    for (var i = 0; i < dob.length; i++) {
         sum = sum + Number(dob.charAt(i));
     }
-    console.log(sum);
+    return sum;
+}
+
+function displayMsg(message) {
+    outputBox.innerText = message;
 }
 
 
